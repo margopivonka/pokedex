984df025ccf54e10fc8d8ce10759612517061559
myd10 <myd10@georgetown.edu>
2020-05-01 17:49:55 -0400

    Updated HTML pages, needs Quiz elements

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -19,3 +19,18 @@ def quiz():
     print("VISTED QUIZ PAGE")
     return render_template("quiz.html")
 
+@home_routes.route("/quiz/submit", methods=["GET", "POST"])
+def quiz_submit():
+    print("SUBMITTED QUIZ RESULTS") #build API using inputs, not sure if we need this tbh
+    #  print("FORM DATA:", dict(request.form)) #> {'full_name': 'Example User', 'email_address': 'me@example.com', 'country': 'US'}
+     # user = dict(request.form)
+    # todo: store in a database or google sheet!
+     # flash(f"User '{user['full_name']}' created successfully!", "success")
+    #flash(f"User '{user['full_name']}' created successfully! (TODO)", "warning")
+    return redirect("/")
+
+@home_routes.route("/animation")
+def animation():
+    print("VISITED ANIMATION")
+    return render_template("animation.html")
+
