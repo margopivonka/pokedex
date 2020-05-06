ceaaae73e83c6ac9f8c4411efc254bc5081c72d9
myd10 <myd10@georgetown.edu>
2020-05-06 13:12:27 -0400

    Travis try 15

--- testing/my_test.py
+++ testing/my_test.py
@@ -3,14 +3,14 @@
 
 from app.my_app import pokemon_info, pokemon_image
 import os
-import requests
-
 
 def test_pokemon_info():
+    import requests
     result = pokemon_info(7)
     assert result["name"] == "squirtle"
 
 def test_pokemon_image():
+    import requests
     result = pokemon_image(7)
     assert result == "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
 
